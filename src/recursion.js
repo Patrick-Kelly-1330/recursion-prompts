0efb833230b0ee7683dd7221bbd54621de14783f
Patrick Kelly <patrick.will.kelly@gmail.com>
2022-12-03 15:42:27 -0800

    complete isEven and sumBelow

--- src/recursion.js
+++ src/recursion.js
@@ -7,92 +7,120 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  // if n is less than 0
+    // return null
+    // otherwise if n is equal to one
+    // return one
+  // return n times calling the factorial on n minus one
   if (n < 0) {
     return null;
-  } else if (n === 0) {
-    return 1;
-  } else if (n === 1) {
+  } else if (n === 1 || n === 0) {
     return 1;
   }
 
-  return n * factorial(n - 1);
+  return n * factorial (n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
   var copy = array.slice();
-
-  if (copy.length === 0) {
-    return 0;
-  } else if (copy.length === 1) {
+  // if array.length is equal to one
+  if (copy.length === 1) {
+    // return first element in the array
     return copy[0];
+  } else if (copy.length === 0) {
+    return 0;
   }
-
-return copy.pop() + sum(copy);
-
+  // return last item in the array plus calling sum on the array minus one element
+  return copy.pop() + sum(copy);
 };
+
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 
 var arraySum = function(array) {
+  // create total
   var total = 0;
-
-  if (array.length === 0) {
-    return 0;
+  // if array is an array
+  if (Array.isArray(array)) {
+    // iterate over the array, for each item
+    array.forEach(function(item) {
+      total += arraySum(item);
+    });
+  } else {
+    total += array;
   }
-
-  // iterate over input array - for each item
-  array.forEach(function(item) {
-    // if item is not an array
-    if (!Array.isArray(item)) {
-      // return sum is equal to sum plus item
-      return total += item;
-      // otherwise
-    } else {
-      // return sum is equal to sum plus calling sum on arraySum
-      return total += arraySum(item);
-    }
-  });
-
+  // return total
   return total;
 };
 
+
 // 4. Check if a number is even.
+
 var isEven = function(n) {
   n = Math.abs(n);
-  // if n is equal to 2
-  if (n === 0) {
-    return true;
-  } else if (n === 2) {
-    return true;
-  } else if (n === 1) {
+
+  if (n === 1) {
     return false;
+  } else if (n === 0) {
+    return true;
   }
+  // call function on n minus two
   return isEven(n - 2);
 };
+
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
-var sumBelow = function(n) {
-  if (n === 0) {
-    return 0;
-  }
 
-  if (n > 0) {
-    return (n - 1) + sumBelow(n - 1);
-  } else {
-    return (n + 1) + sumBelow(n + 1);
+
+var sumBelow = function(n) {
+  // if n is equal to 0
+  if (n === 0) {
+    // return 0
+    return 0;
+  } else if (n < 0) {
+    return n + 1 + sumBelow(n + 1);
   }
+  // return n minus 1 plus calling function on n - 1
+  return n - 1 + sumBelow(n - 1);
 };
 
-console.log(sumBelow(7));
+
+
+
+
+
+
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x === y || y === x + 1) {
+    return [];
+  }
+  if (y > x) {
+    // if x plus one is equal to y
+    if (y - x === 2) {
+      // return the result
+      return [x + 1];
+    } else {
+      var result = range(x, y - 1);
+      result.push(y - 1);
+      return result;
+    }
+  } else if (x > y) {
+    if (x - y === 2) {
+      return [x - 1];
+    } else {
+      var result2 = range(x, y + 1);
+      result2.push(y + 1);
+      return result2;
+    }
+  }
 };
 
 // 7. Compute the exponent of a number.
@@ -101,7 +129,26 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+
+  if (exp > 0) {
+    // if exponent is equal to 1
+    if (exp === 1) {
+    // return base
+    return base;
+    }
+    // mutlply base times base exp - 1 times
+    return base * exponent(base, exp - 1);
+  }
+
+  if (exp < 0) {
+    return 1 / base * (exponent(base, exp + 1));
+  }
 };
+
+
 
 // 8. Determine if a number is a power of two.
 // powerOfTwo(1); // true
