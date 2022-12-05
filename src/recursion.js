d02fc4a12397eac0794ef0f600103b62124720ae
Patrick Kelly <patrick.will.kelly@gmail.com>
2022-12-05 14:17:53 -0800

    complete palindrome function

--- src/recursion.js
+++ src/recursion.js
@@ -173,6 +173,7 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase();
   // if length of string is equal to 0 or 1
   if (string.length === 0 || string.length === 1) {
     return true;
