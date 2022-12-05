d974536fc9ce5a93627939819cf0e1d75febb2fe
Patrick Kelly <patrick.will.kelly@gmail.com>
2022-12-05 14:17:10 -0800

    Complete reverse function

--- src/recursion.js
+++ src/recursion.js
@@ -158,10 +158,32 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  // create result string
+  var reversed = '';
+  // if string length equal to 0
+  if (string.length === 0) {
+    // return result
+    console.log('result ', reversed);
+    return reversed;
+  }
+  reversed += string.slice(-1);
+  // call function on string minus last character
+  return reversed += reverse(string.slice(0, -1));
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  // if length of string is equal to 0 or 1
+  if (string.length === 0 || string.length === 1) {
+    return true;
+  }
+
+  if (string[0] === string[string.length - 1]) {
+    console.log(string);
+    return palindrome(string.slice(1, -1));
+  }
+
+  return false;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
